fc8f1449f9ce87159884006306c1b43eefa4c88e
Anzola Fabio <anzola.fabio@gmail.com>
2021-08-07 00:37:18 +0200

    Database model template

--- server.py
+++ server.py
@@ -4,6 +4,13 @@ from functools import wraps
 
 app = Flask(__name__)
 
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  public_id = db.Column(db.String(50), unique=True)
+  name = db.Column(db.String(50))
+  password = db.Column(db.String(80))
+  admin = db.Column(db.Boolean)
+
 def token_required(f):
   @wraps(f)
   def decorated(*args, **kwargs):
