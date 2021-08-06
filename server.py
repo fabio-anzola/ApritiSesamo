456f65de5257e5f4d73a5f747678ebeaed774096
Anzola Fabio <anzola.fabio@gmail.com>
2021-08-07 00:35:54 +0200

    Basic token security

--- server.py
+++ server.py
@@ -1,8 +1,36 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request, jsonify
+
+from functools import wraps
 
 app = Flask(__name__)
 
+def token_required(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    token = None
+
+    if 'x-access-token' in request.headers:
+      token = request.headers['x-access-token']
+
+    if not token:
+      return jsonify({'message': 'Token invalid or missing'}), 401
+
+    try:
+      data = jwt.decode(
+        token, app.config['SECRET_KEY'], algorithms=["HS256"])
+
+      current_user = User.query.filter_by(
+        public_id=data['public_id']).first()
+
+    except:
+      return jsonify({'message': 'Token invalid or missing'}), 401
+
+    return f(current_user, *args, **kwargs)
+
+  return decorated
+
 @app.route('/')
+@token_required
 def index():
 	return 'Hello, Flask!'
 
