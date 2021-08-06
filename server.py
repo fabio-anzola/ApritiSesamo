003021ca786d75a02bf269dc226662e714b4f274
Anzola Fabio <anzola.fabio@gmail.com>
2021-08-07 00:41:48 +0200

    Add sqlite db name

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///accesscontrol.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
