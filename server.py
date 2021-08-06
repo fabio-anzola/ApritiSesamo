6058b2885c82bad890f8473c19b58ddb4c5ba4ed
Anzola Fabio <anzola.fabio@gmail.com>
2021-08-07 00:30:20 +0200

    Statically serve robots.txt

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return 'Hello, Flask!'
+
+@app.route("/robots.txt")
+def robots():
+    return send_from_directory("static", "robots.txt")
 
 if __name__ == '__main__':
 	app.run(debug=True)
